28ff376dacfefb331b2cb9f27df3d3157e82fbb4
Philippe Veber <philippe.veber@gmail.com>
2019-11-11 18:31:06 +0100

    better output

--- owl_perfs.ml
+++ owl_perfs.ml
@@ -12,9 +12,9 @@ let mat_mat_mul n =
   let c = lacaml_jk69 n in
   let c' = owl_jk69 n in
   let lacaml =
-    Bench.Test.create ~name:"Lacaml_mat_mat_mul" (fun () -> Lacaml.D.gemm c c)
+    Bench.Test.create ~name:(sprintf "lacaml-mat-mat-mul-%d" n) (fun () -> Lacaml.D.gemm c c)
   and owl =
-    Bench.Test.create ~name:"Owl_mat_mat_mul" (fun () -> Owl.Mat.dot c' c') in
+    Bench.Test.create ~name:(sprintf "owl-mat-mat-mul-%d" n) (fun () -> Owl.Mat.dot c' c') in
   Bench.make_command [lacaml ; owl]
 
 let mat_vec_mul n =
@@ -22,19 +22,19 @@ let mat_vec_mul n =
   let c' = owl_jk69 n in
   let v = Lacaml.D.Vec.init n float in
   let v' = Owl.Mat.init_2d n 1 (fun i _ -> float i) in
-  let lacaml = Bench.Test.create ~name:"Lacaml_mat_vec_mul" (fun () -> Lacaml.D.gemv c v) in
-  let owl = Bench.Test.create ~name:"Owl_mat_vec_mul" (fun () -> Owl.Mat.dot c' v') in
+  let lacaml = Bench.Test.create ~name:(sprintf "lacaml-mat-vec-mul-%d" n) (fun () -> Lacaml.D.gemv c v) in
+  let owl = Bench.Test.create ~name:(sprintf "owl-mat-vec-mul-%d" n) (fun () -> Owl.Mat.dot c' v') in
   Bench.make_command [lacaml ; owl]
 
 let vec_init n =
   let k = n / 2 in
   let f i = if i = k then 1. else 0. in
   let lacaml =
-    Bench.Test.create ~name:"Lacaml_vec_init" (fun () ->
+    Bench.Test.create ~name:(sprintf "lacaml-vec-init-%d" n) (fun () ->
         Lacaml.D.Vec.init n f
       )
   and owl =
-    Bench.Test.create ~name:"Owl_vec_init" (fun () ->
+    Bench.Test.create ~name:(sprintf "owl-vec-init-%d" n) (fun () ->
         Owl.Arr.init [|n|] f
       )
   in
